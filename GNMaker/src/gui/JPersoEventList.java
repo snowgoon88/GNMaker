9b6e69228ced5f15b0dd9dae1c00e50a5604569f
Alain Dutech <alain.dutech@loria.fr>
2013-08-08 12:44:49 +0200

    JPersoEventList : added ExpandAll Button.

--- GNMaker/src/gui/JPersoEventList.java
+++ GNMaker/src/gui/JPersoEventList.java
@@ -3,20 +3,23 @@
  */
 package gui;
 
+
 import java.awt.event.ActionEvent;
-import java.util.HashMap;
-import java.util.Map;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
+import utils.GraphicHelper;
+
 import net.miginfocom.swing.MigLayout;
 
 import data.Event;
-import data.Perso;
 import data.Event.PersoEvent;
 
 /**
@@ -26,7 +29,7 @@ import data.Event.PersoEvent;
  * TODO Add a new PersoEvent
  * TODO Remove a given PersoEvent
  * Expand a PersoEvent (LeftClick on JPersoEvent)
- * TODO Expand All
+ * Expand All
  * TODO Dump all PersoEvent
  * 
  * @author snowgoon88@gmail.com
@@ -38,10 +41,20 @@ public class JPersoEventList {
 	/** JPanel comme Component */
 	public JPanel _component;
 	
+	/** ExpandAll Button */
+	JButton _expandAllBtn;
+	
+	/** Class for helping in designing GUI */
+	ImageIcon _iconClosed = GraphicHelper.createImageIcon(this,"book-closed_32x32.png", "");
+	ImageIcon _iconOpen = GraphicHelper.createImageIcon(this,"book-open_32x32.png", "");
+	
 	/** Map de description */
-	HashMap<Perso,JTextArea> _descArea;
+	ArrayList<JTextArea> _descArea;
 	/** Map de name */
-	HashMap<Perso,JLabel> _nameLabel;
+	ArrayList<JLabel> _nameLabel;
+	
+	/** Status Flag pour AllDescExpand */
+	boolean _allDescVisible = true;
 
 	/**
 	 * Création avec un Event comme modèle (MVC).
@@ -54,8 +67,8 @@ public class JPersoEventList {
 	
 	void buildGUI() {
 		// Internal components list
-		_nameLabel = new HashMap<Perso,JLabel>();
-		_descArea = new HashMap<Perso,JTextArea>();
+		_nameLabel = new ArrayList<JLabel>();
+		_descArea = new ArrayList<JTextArea>();
 		
 		// Main Panel avec un BoxLayout
 		_component = new JPanel();
@@ -72,6 +85,14 @@ public class JPersoEventList {
 				"2*indent", // Column constraints
 				""); // Row constraints);
 		persoPanel.setLayout(persoLayout);
+		_expandAllBtn = new JButton();
+		_expandAllBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setAllDescVisible(!_allDescVisible);
+			}
+		});
+		persoPanel.add(_expandAllBtn);
 		
 		// Description/Body pour chaque perso.
 		JPanel descPanel = new JPanel();
@@ -85,38 +106,24 @@ public class JPersoEventList {
 			JPersoEvent persoBtn = new JPersoEvent(p._perso, _evt);
 			persoPanel.add( persoBtn._btn );
 			JLabel nameLabel = new JLabel(p._perso.SDump());
-			_nameLabel.put(p._perso, nameLabel);
+			_nameLabel.add(nameLabel);
 			descPanel.add(nameLabel, "wrap"); // next est sur une autre ligne
 			
 			JTextArea descArea = new JTextArea(p._desc);
-			_descArea.put(p._perso, descArea);
+			_descArea.add(descArea);
 			descPanel.add(descArea, "wrap, growx, growy"); // prend place, prochain sur autre ligne
 			
 			// Attache la bonne action 
 			persoBtn._leftClickAction = new ExpandDescAction("Détaille", null, "Détaille "+p._perso._name, null,
 					nameLabel, descArea);
 		}
+		setAllDescVisible(true);
+		
 		_component.add(persoPanel);
 		_component.add(descPanel);
 	}
 	
 
-//	/**
-//	 * Reset Arm.
-//	 */
-//	@SuppressWarnings("serial")
-//	class ResetAction extends AbstractAction {
-//		public ResetAction(String text, ImageIcon icon, String desc, Integer mnemonic) {
-//			super(text, icon);
-//			putValue(SHORT_DESCRIPTION, desc);
-//			putValue(MNEMONIC_KEY, mnemonic);
-//		}
-//		@Override
-//		public void actionPerformed(ActionEvent e) {
-//			reset( _ang0SpinModel.getNumber().doubleValue(),
-//					_ang1SpinModel.getNumber().doubleValue());
-//		}
-//	}
 	@SuppressWarnings("serial")
 	/**
 	 * Change l'état visible du descriptif d'un PersoEvent (label+desc).
@@ -140,5 +147,26 @@ public class JPersoEventList {
 			_label.revalidate();
 			_desc.revalidate();
 		}
+	}
+	
+	/**
+	 * Rend tous les Descriptifs de PersoEvent (label+Desc) visibles ou non.
+	 * @param aFlag true pour allVisible.
+	 */
+	void setAllDescVisible( boolean aFlag) {
+		_allDescVisible = aFlag;
+		for (JLabel l : _nameLabel) {
+			l.setVisible(_allDescVisible);
+		}
+		for (JTextArea a : _descArea) {
+			a.setVisible(_allDescVisible);
+		}
+		if (_allDescVisible) {
+			_expandAllBtn.setIcon(_iconOpen);
+		}
+		else {
+			_expandAllBtn.setIcon(_iconClosed);
+		}
+		_component.revalidate();
 	}
 }
